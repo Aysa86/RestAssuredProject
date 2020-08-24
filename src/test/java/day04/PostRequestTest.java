85b79e309fb7b4dd2dfe1d2ba0fbc36485db3ec1
Aysa Dzhalaeva <adzhalaeva@gmail.com>
2020-08-24 14:44:26 -0400

    Update

--- src/test/java/day04/PostRequestTest.java
+++ src/test/java/day04/PostRequestTest.java
@@ -1,7 +1,73 @@
 package day04;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import com.github.javafaker.Faker;
+import io.restassured.RestAssured;
 import static io.restassured.RestAssured.* ;
-import static io.restassured.matcher.RestAssuredMatchers.* ;
 import static org.hamcrest.Matchers.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
 public class PostRequestTest {
+
+    @BeforeAll
+    public static void setUp() {
+
+        RestAssured.baseURI = "http://100.25.162.89:8000";
+        RestAssured.basePath = "/api";
+    }
+
+    @DisplayName("Post request with String body")
+    @Test
+    public void testPostWithStringBody(){
+
+        // lets try to get random names rather than same ADAM each time
+        Faker faker = new Faker();
+        String randomName = faker.name().firstName();
+        System.out.println("randomName = " + randomName);
+
+        String bodyString = "{\n" +
+                "  \"name\"  : \"" + randomName+ "\",\n" +
+                "  \"gender\": \"Female\",\n" +
+                "  \"phone\": 6234567890\n" +
+                "}";
+
+        given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(bodyString).
+        when()
+                .post("/spartans").
+        then()
+                .log().all()
+                .statusCode(201)
+                .body("data.name", is(randomName));
+    }
+    @DisplayName("Posting with external json file")
+    @Test
+    public void testPostWithExternalFile(){
+
+        // create a file object that point to spartan.json you just added
+        // so we can use this as body in post request
+        File file1 = new File("spartan.json");
+
+        given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(file1).
+        when()
+                .post("/spartans").
+        then()
+                .log().all()
+                .statusCode(201)
+                .body("data.name", is("From file"));
+
+    }
 
 }
