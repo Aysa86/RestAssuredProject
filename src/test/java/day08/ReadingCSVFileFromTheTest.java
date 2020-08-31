ec09ce472e9e41c6067ebed1699883adadb27944
Aysa Dzhalaeva <adzhalaeva@gmail.com>
2020-08-31 15:26:47 -0400

    Update

--- src/test/java/day08/ReadingCSVFileFromTheTest.java
+++ src/test/java/day08/ReadingCSVFileFromTheTest.java
@@ -31,13 +31,35 @@ public class ReadingCSVFileFromTheTest {
 ///  in the meantime add 3rd parameter to your test called int result
 /// assert that num1 + num2  = result
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "Iteration {index} -> {0} + {1} = {2} ")
     @CsvFileSource(resources = "/numbers.csv", numLinesToSkip = 1)
     public void ReadingCSVFile(int num1, int num2, int result){
 
         assertEquals(result, num1 + num2, "Something went WRONG");
-
-
     }
+
+    // What if I want some custom name
+    // @ParameterizedTest(name = "Some custom name here")
+    // How do I refer the row number in my csv file
+       // you can refer row number using {index} in the name String
+    // How do I refer the column data in my display name
+       // // in the name String we can add {yourColumnIndexHere}
+    //    // {0} for first column
+    //    // {1} for second column
+    //    // {2} for third column
+    // WHERE DO I ADD the display name to start with ?  add it to @ParameterizedTest (name = " here")
+    // we want display like this so we know exactly what we tested in each iteration
+    // Row1 : 5+4=9
+    // Row2 : 4+7=1
+
+
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/numbers.csv", numLinesToSkip = 1)
+    public void testAdditional(int num1, int num2, int result){
+
+        assertEquals(result, num1 + num2, "Something went WRONG");
+    }
+
 
 }
