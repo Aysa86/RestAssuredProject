d37b65cf5e3d97234fd108e39660f65f3689f40e
Aysa Dzhalaeva <adzhalaeva@gmail.com>
2020-09-08 16:55:43 -0400

    Update

--- src/test/java/day12/XMLResponseTestMovieDB.java
+++ src/test/java/day12/XMLResponseTestMovieDB.java
@@ -2,8 +2,14 @@ package day12;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.is;
@@ -32,6 +38,24 @@ public class XMLResponseTestMovieDB {
                    .body("root.movie.@rated", is("PG-13"))
                    .body("root.movie.@released", is("05 May 2017"))
                    .body("root.movie.@runtime", is("136 min"));
+
+       }
+
+       @DisplayName("Getting many movies and extracting attributes to the List")
+       @Test
+       public void testGettingAttributesAsList(){
+       Response response = given()
+                   .log().all()
+                   .baseUri("http://www.omdbapi.com/")
+                   .queryParam("apikey", "de46cef4")
+                   .queryParam("s", "superman")
+                   .queryParam("r", "xml").
+           when()
+                   .get();
+
+           XmlPath xmlPath = response.xmlPath();
+           List<String> superManMovieList = xmlPath.getList("root.resuly.@title");
+           System.out.println("superManMovieList = " + superManMovieList);
 
        }
 
